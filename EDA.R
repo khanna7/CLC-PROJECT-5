27caa67694cf46e17e25ed3ac0baa470c518bf32
Aditya Khanna <khanna7.work@gmail.com>
2022-03-09 13:10:42 -0500

    Produce Table 2 components, covariates for Tables 3-5

--- EDA.R
+++ EDA.R
@@ -36,6 +36,32 @@ table(dt$Screen1) # see CADRE CLC Data Project5/Qualtrics Codebooks/Coronavirus_
 table(dt$Screen2) #sex assigned at birth, 1=male, 2=female
 table(dt$Screen2, exclude = NULL)/sum(table(dt$Screen2))
 
+table(dt$DEMO25_1, exclude = NULL) #gender identity = man
+table(dt$DEMO25_2, exclude = NULL) #gender identity = woman
+table(dt$DEMO25_3, exclude = NULL) #gender identity = non-binary
+table(dt$DEMO25_4, exclude = NULL) #gender identity = transgender
+table(dt$DEMO25_5, exclude = NULL) #gender identity = none of the above
+table(dt$DEMO25_7, exclude = NULL) #gender identity = prefer not to answer
+
+gender = rep(NA, n)
+
+man <- which(dt$DEMO25_1 == 1)
+woman <- which(dt$DEMO25_2 == 1)
+non_binary <- which(dt$DEMO25_3 == 1)
+trans <- which(dt$DEMO25_4 == 1)
+no_gender <- which(dt$DEMO25_5 == 1)
+missing_gender <- which(dt$DEMO25_7 == 1)
+
+gender[man] <- 1
+gender[woman] <- 2
+gender[non_binary] <- 3
+gender[trans] <- 4
+gender[no_gender] <- 5
+gender[missing_gender] <- 7
+
+table(gender, exclude = NULL)
+
+
 ## race 
   # DEMO4 What is your race? [Choose all that apply]:
   # ▢	White  (1) 
@@ -62,7 +88,7 @@ table(dt$DEMO4_3, exclude = NULL)
  #- is there an aggregate variable?
  # see below for "DEMO4SUM"?
 
-table(dt$DEMO4SUM) #code interpretation is not clear to me
+table(dt$DEMO4SUM, exclude = NULL) #code interpretation is not clear to me
 
 # screening variable below:
 table(dt$Screen3, exclude = NULL)/(sum(table(dt$Screen3, exclude = NULL)))
@@ -87,6 +113,20 @@ table(dt$DEMO3_3, exclude = NULL) #SEE ABOVE QTS FOR RACE
 table(dt$Screen4, exclude = NULL)
 table(dt$Screen4, exclude = NULL)/(sum(table(dt$Screen4, exclude = NULL)))
 
+ethnicity = rep(NA, n)
+not_hispanic <- which(dt$DEMO3_1 == 1)
+hispanic <- which(is.na(dt$DEMO3_1))
+mexican <- which(dt$DEMO3_2 == 1)
+puerto_rican <- which(dt$DEMO3_3 == 1)
+cuban <- which(dt$DEMO3_4 == 1)
+another_latinx <- which(dt$DEMO3_5 == 1)
+dont_know_ethnicity <- which(dt$DEMO3_6 == 1)
+
+ethnicity[not_hispanic] <- 0
+ethnicity[hispanic] <- 1 #binary classification
+
+table(ethnicity, exclude = NULL)
+
 ## income
   # o	Less than $25,000  (4) 
   # o	$25,000 - $34,999  (5) 
@@ -98,6 +138,7 @@ table(dt$Screen4, exclude = NULL)/(sum(table(dt$Screen4, exclude = NULL)))
   # o	$200,000 and above  (11) 
 table(dt$DEMO20, exclude=NULL)
 table(dt$DEMO20, exclude=NULL)/nrow(dt)
+income=dt$DEMO20
 
 ## essential worker
   # o	Yes  (1) 
@@ -105,6 +146,17 @@ table(dt$DEMO20, exclude=NULL)/nrow(dt)
   # o	Not Sure  (3) 
 table(dt$LSQ3, exclude = NULL)
 table(dt$LSQ3, exclude = NULL)/n
+
+essential_worker <- rep(NA, n)
+yes_essential_worker <- which(dt$LSQ3 == 1)
+no2_essential_worker <- which(dt$LSQ3 == 2)
+no3_essential_worker <- which(dt$LSQ3 == 3)
+
+essential_worker[yes_essential_worker] <- 1
+essential_worker[no2_essential_worker] <- 0 #dichotomize
+essential_worker[no3_essential_worker] <- 0
+
+table(essential_worker, exclude = NULL)
 
 ## substance use in past 7 days
 ## QT FOR ALL: How did we find the daily vs non-daily categorization?
@@ -155,15 +207,38 @@ table(dt$LSQ12b1, exclude = NULL)
 nasal_pos <- which(dt$LSQ12a1 == 1)
 blood_pos <- which(dt$LSQ12b1 == 1)
 
+length(nasal_pos)
+length(blood_pos)
+identical(nasal_pos, blood_pos)
+
 pos_covid_test <- unique(c(nasal_pos,  blood_pos))
 length(pos_covid_test)
 
+covid_test <- rep(0, n)
+covid_test[pos_covid_test] <- 1
+table(covid_test, exclude = NULL) #dichotomize
 
 # TAB 2: Characteristics of the participants ---------------------------
 # See above
 
+# Covariates for Tables 3-5 ---------------------------
+
+cov_dt <- cbind.data.frame(
+  gender,
+  ethnicity,
+  essential_worker,
+  income,
+  covid_test
+)
+dim(cov_dt)
+
+cov_dt_na.omit <- na.omit(cov_dt)
+dim(cov_dt_na.omit)
+
+# Primary Outcome (Table 3) ---------------------------
 
 # TAB 3: GLM, outcome: substance use, COVs: CDC guideline adherence ---------------------------
+
 
 
 # TAB 4: Logistic regression of SU vs any COVID-19 testing ---------------------------
