6faef768ed9d95c22f4cc1c53bfcb713b220fe41
Aditya Khanna <khanna7.work@gmail.com>
2023-03-23 17:20:29 -0400

    WIP: add ego attrs to g

--- visualize-networks.R
+++ visualize-networks.R
@@ -2,6 +2,7 @@
 
 
 ## Clear the workspace ----------
+
 rm(list=ls())
 
 
@@ -21,8 +22,16 @@ library(ggraph)
 
 ## Load data ----------
 
+### extract egos (i.e. participants) consenting to give SNS data, 
+### and their attributes
+
+network_env <- readRDS("network_objects.rds")
+sns_consenting_dt <- as.data.table(network_env$sns_consenting_dt)
+
+### extract attributes of adult alters (i.e., network members)
 merged_network_participant_env <- readRDS("merged_network_participant_objects.rds")
 sns_dt_long_merged_ego_characteristics <- merged_network_participant_env$sns_dt_long_merged_ego_characteristics
+
 
 ## Create a network object ----------
 
@@ -32,7 +41,7 @@ g <- igraph::graph_from_edgelist(edge_list)
 
 ### investigate the structure of g
 igraph::vertex_attr(g)
-head(V(g)$name)
+head(V(g)$name, 20)
 
 
 ### count the number of clusters in g
@@ -101,4 +110,21 @@ ggraph(sub_g, layout = "kk") +
 
 
 ## Visualize clustering on one "trait" of interest ----------
-### vh1 in the egos vs 
+### cdc_avg_out_adequate in the egos vs 
+setnames(sns_consenting_dt, "MTURK1", "MTURKID")
+cols_of_interest <- "cdc_avg_out_adequate"
+selected_rows <- sns_consenting_dt[MTURKID %in% V(g)$name, ..cols_of_interest]
+attr_values <- rep(NA, vcount(g))
+attr_values[match(selected_rows$MTURKID, V(g)$name)] <- selected_rows[[cols_of_interest]]
+
+g <- set_vertex_attr(g, name = cols_of_interest, value = attr_values)
+g <- set_vertex_attr(g, name = "cdc_avg_out_adequate", value = NA)
+
+matching_nodes <- V(g)[name %in% sns_consenting_dt$MTURKID]
+g <- set_vertex_attr(g, name = "cdc_avg_out_adequate", 
+                     value = ifelse(V(g)$name %in% matching_nodes$name, 
+                                    sns_consenting_dt[MTURKID %in% matching_nodes$name, "cdc_avg_out_adequate"],
+                                    NA))
+
+
+
