0ab0721cef2e7fa582d554a296d11b17028bf5fb
Aditya Khanna <khanna7.work@gmail.com>
2023-05-07 15:34:02 -0400

    added SN32 - alter encouraged testing

--- visualize-networks.R
+++ visualize-networks.R
@@ -395,6 +395,7 @@ g <- igraph::graph_from_edgelist(edge_list)
   
    
   
+  
 # Analyze alter knowing anyone who died of COVID-19  ----------
   
   compute_ego_alter_attribute_proportions_SN29 <- function(vertex_dt, edge_dt, attribute_name) {
@@ -442,4 +443,55 @@ g <- igraph::graph_from_edgelist(edge_list)
   cor_cdc_missing <- cor(ego_proportions_df$cdc_avg_out, ego_proportions_df$proportion_missing, use = "complete.obs")
   
   list(cor_cdc_yes = cor_cdc_yes, cor_cdc_no = cor_cdc_no, cor_cdc_missing = cor_cdc_missing)
+  
+  
+# Analyze alter encouraging testing ----------
+  
+  compute_ego_alter_attribute_proportions_SN32 <- function(vertex_dt, edge_dt, attribute_name) {
+    egos <- unique(edge_dt$MTURKID)
+    ego_alter_attribute_proportions <- list()
+    
+    for (ego in egos) {
+      alter_ids <- edge_dt[edge_dt$MTURKID == ego,]$alterID
+      alter_data <- vertex_dt[vertex_dt$name %in% alter_ids, c("name", attribute_name), with = FALSE]
+      
+      yes_count <- sum(alter_data[[attribute_name]] == 1, na.rm = TRUE)
+      no_count <- sum(alter_data[[attribute_name]] == 2, na.rm = TRUE)
+      missing_count <- sum(is.na(alter_data[[attribute_name]]))
+      total_count <- yes_count + no_count + missing_count
+      
+      proportion_yes <- yes_count / total_count
+      proportion_no <- no_count / total_count
+      proportion_missing <- missing_count / total_count
+      
+      ego_alter_attribute_proportions[[as.character(ego)]] <- list(
+        proportion_yes = proportion_yes,
+        proportion_no = proportion_no,
+        proportion_missing = proportion_missing
+      )
+    }
+    
+    return(ego_alter_attribute_proportions)
+  }
+  
+  ego_alter_stats_SN32 <- compute_ego_alter_attribute_proportions_SN32(vertex_dt, edge_dt, "SN32")
+  
+  ## compute correlations with %alters reporting knowing someone who died of COVID-19
+  ego_data <- vertex_dt[!grepl("_", vertex_dt$name),]
+  
+  ego_proportions_df_SN32 <- data.frame(# create data frame
+    MTURKID = names(ego_alter_stats_SN32),
+    cdc_avg_out = ego_data$cdc_avg_out.x,
+    proportion_yes = sapply(ego_alter_stats_SN32, function(x) x$proportion_yes),
+    proportion_no = sapply(ego_alter_stats_SN32, function(x) x$proportion_no),
+    proportion_missing = sapply(ego_alter_stats_SN32, function(x) x$proportion_missing)
+  )
+  
+  cor_cdc_yes_SN32 <- cor(ego_proportions_df_SN32$cdc_avg_out, ego_proportions_df_SN32$proportion_yes, use = "complete.obs")
+  cor_cdc_no_SN32 <- cor(ego_proportions_df_SN32$cdc_avg_out, ego_proportions_df_SN32$proportion_no, use = "complete.obs")
+  cor_cdc_missing_SN32 <- cor(ego_proportions_df_SN32$cdc_avg_out, ego_proportions_df_SN32$proportion_missing, use = "complete.obs")
+  
+  list(cor_cdc_yes = cor_cdc_yes, cor_cdc_no = cor_cdc_no, cor_cdc_missing = cor_cdc_missing)
+  
+  
   
